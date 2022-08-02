a26ced9a370c44bb15db431302e29b52ee912b51
Sam Lijin <sam@trunk.io>
2022-08-02 14:00:59 -0700

    trunk fmt

--- linters/pragma-once/fix.sh
+++ linters/pragma-once/fix.sh
@@ -5,8 +5,7 @@ LINT_TARGET="${1}"
 
 # NOTE(sam): implementing this with `batch: true` is a bit non-trivial (impl would be a
 # slightly-more-than-trivial awk program that modifies files in place).
-if ! grep --quiet '^#pragma once$' "${LINT_TARGET}"
-then
+if ! grep --quiet '^#pragma once$' "${LINT_TARGET}"; then
   echo "#pragma once"
 fi
 
