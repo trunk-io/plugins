0a59de4ecb788725fb826db62bc0e90e63953df8
Tyler Jang <tyler@trunk.io>
2022-12-07 14:01:46 -0800

    fix unbound variable

--- locate_trunk.sh
+++ locate_trunk.sh
@@ -2,20 +2,17 @@
 
 set -euo pipefail
 
-trunk_path="${INPUT_TRUNK_PATH}"
-if [[ -z ${trunk_path} ]]; then
-  if [[ -f .trunk/bin/trunk && -x .trunk/bin/trunk ]]; then
-    trunk_path=.trunk/bin/trunk
-  elif [[ -f tools/trunk && -x tools/trunk ]]; then
-    trunk_path=tools/trunk
-  elif [[ -f trunk && -x trunk ]]; then
-    trunk_path=./trunk
-  else
-    tmpdir="$(mktemp -d)"
-    curl -fsSL https://trunk.io/releases/trunk -o "${tmpdir}/trunk"
-    chmod u+x "${tmpdir}/trunk"
-    trunk_path="${tmpdir}/trunk"
-    echo "TRUNK_TMPDIR=${tmpdir}" >>"${GITHUB_ENV}"
-  fi
+if [[ -f .trunk/bin/trunk && -x .trunk/bin/trunk ]]; then
+  trunk_path=.trunk/bin/trunk
+elif [[ -f tools/trunk && -x tools/trunk ]]; then
+  trunk_path=tools/trunk
+elif [[ -f trunk && -x trunk ]]; then
+  trunk_path=./trunk
+else
+  tmpdir="$(mktemp -d)"
+  curl -fsSL https://trunk.io/releases/trunk -o "${tmpdir}/trunk"
+  chmod u+x "${tmpdir}/trunk"
+  trunk_path="${tmpdir}/trunk"
+  echo "TRUNK_TMPDIR=${tmpdir}" >>"${GITHUB_ENV}"
 fi
 echo "TRUNK_PATH=${trunk_path}" >>"${GITHUB_ENV}"
