626a6ac75e66289bb862428992ee66f3c58abbe4
Tyler Jang <tyler@trunk.io>
2022-12-13 16:35:00 -0800

    added detekt

---
+++ linters/detekt/detekt_test.ts
@@ -0,0 +1,17 @@
+import * as fs from "fs";
+import * as path from "path";
+import { linterCheckTest } from "tests";
+import { TrunkDriver } from "tests/driver";
+
+// Running check on the input manually requires the existence of a top level .detekt.yaml
+const preCheck = (driver: TrunkDriver) => {
+  if (driver.sandboxPath) {
+    // trunk-ignore(semgrep): driver.sandboxPath is generated deterministically and is safe
+    fs.writeFileSync(path.resolve(driver.sandboxPath, ".detekt.yaml"), "");
+  }
+};
+
+linterCheckTest({ linterName: "detekt", namedTestPrefixes: ["basic_detekt"], preCheck });
+
+// detekt-explicit has no default settings, leading to an empty result
+linterCheckTest({ linterName: "detekt-explicit", namedTestPrefixes: ["basic_explicit"], preCheck });
