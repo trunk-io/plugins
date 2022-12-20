613c5b1ef2a94945f23f37358f43ac8798f33678
Tyler Jang <tyler@trunk.io>
2022-12-19 16:02:23 -0800

    add test for coverage

---
+++ tests/test_coverage_test.ts
@@ -0,0 +1,43 @@
+import { REPO_ROOT } from "tests/utils";
+import fs from "fs";
+import { execSync } from "child_process";
+import path from "path";
+
+// TODO(Tyler): Burndown this list.
+const excludedLinters: string[] = ["codespell", "cspell", "nancy", "oxipng", "sqlfmt", "trivy"];
+
+describe("All linters must have tests", () => {
+  // Find all tests detected by jest
+  const stdout = execSync("npm run test -- --listTests", { cwd: REPO_ROOT }).toString();
+  const testFiles = stdout
+    .split("\n")
+    .filter((file) => file.startsWith("/"))
+    .map((file) => path.relative(REPO_ROOT, file));
+
+  // Key the tests by their linter subdirectory
+  const testDirMap = testFiles.reduce((accumulator: Map<string, string[]>, file: string) => {
+    const linterSubdir = file.match(/linters\/[^\/]+/);
+    if (linterSubdir) {
+      const matches = accumulator.get(linterSubdir[0]) ?? [];
+      accumulator.set(linterSubdir[0], [...matches, file]);
+    }
+    return accumulator;
+  }, new Map<string, string[]>());
+  const testDirObject = Object.fromEntries(testDirMap);
+
+  // Find all linter subdirectories
+  const linterDir = path.resolve(REPO_ROOT, "linters");
+  const linters = fs
+    .readdirSync(linterDir)
+    .filter((file) => fs.lstatSync(path.resolve(linterDir, file)).isDirectory());
+
+  // Assert that each linter subdirectory has a test (excluding an explicit subset)
+  linters
+    .filter((linter) => !excludedLinters.includes(linter))
+    .forEach((linter) => {
+      it(linter, () => {
+        const linterSubdir = path.join("linters", linter);
+        expect(testDirObject).toHaveProperty(linterSubdir);
+      });
+    });
+});
