e55ad85968ce219c93692d2376178d3283cbafe1
Tyler Jang <tyler@trunk.io>
2022-12-20 11:02:50 -0800

    Improve testing workflow (#68)
    Changes targeted by this PR:
    - Avoid snapshot assertions failing when it's a new version that doesn't
    have a snapshot. This includes removing the requirement to specify
    output files, and now snapshots are always autogenerated. See comment in
    `linterCheckTest()` for specifics on this design. This decision was
    rooted in a couple factors, namely:
    - As identified in #65, the lack of a snapshot for a specific version
    was causing unnecessary failures.
    - Linters can still change their messages, outputs, etc (as sqlfluff did
    yesterday). This is something that we don't want to cause too much
    friction, but we still want to call it out as a noticeable change. If
    this happens during someone's workflow (should still be relatively
    infrequent), now simply run the specific command in tests/readme.md and
    validate expected output. _We may want to highlight this specific
    command better or with more visibility._
    - The JSON assertions and snapshot code were a bit redundant and
    unnecessarily complex. This should simplify and be flexible enough for
    the near future. If we want any additional assertions, we can do so via
    the `postCheck` hook.
    - Make tests run in parallel. This also simplifies the setup for the
    `config_check_test.ts`. It's a tad redundant, but its presence makes
    triaging problems a bit easier.
    - Attempt to reduce flakiness (the parallelism appears to reduce
    flakiness to about 1-3%--it'd be nice to have analytics on this). I'll
    continue investigating alternatives for dealing with the daemon
    lifetime.
    
    Average test time right now locally with these 4 tests is about 10
    seconds in total :)
    They're about a minute in total on CI because of the need to download
    and install linters.
    
    Remaining todos (unordered):
    - Investigate daemon lifetime changes
    - Modify sandboxing to shadow tree from root
    - Use debugging library in testing
    - Add contributing.md
    - Add option for specifying a range of linter versions (ideally for
    nightlies)
    - Migrate rest of definitions

--- tests/utils/landing_state.ts
+++ tests/utils/landing_state.ts
@@ -1,82 +1,41 @@
 import { sort } from "fast-sort";
 import path from "path";
-import { FileIssue, LandingState, LintAction, TaskFailure } from "tests/types";
+import { LandingState, LintAction, TaskFailure } from "tests/types";
 import { REPO_ROOT } from "tests/utils";
 
-const extractFileIssueFields = (
-  { file, line, column, message, code, level, linter, targetType, issueUrl, ..._rest }: FileIssue,
-  skip_message: boolean
-): FileIssue =>
-  skip_message
-    ? {
-        file,
-        line,
-        column,
-        // omitted message for assertions
-        code,
-        level,
-        linter,
-        targetType,
-        issueUrl,
-      }
-    : {
-        file,
-        line,
-        column,
-        message,
-        code,
-        level,
-        linter,
-        targetType,
-        issueUrl,
-      };
-
-// Also de-dupe, since sometimes we will have discrepancies in the count for multiple commands
 const extractLintActionFields = ({
-  paths,
-  linter,
-  parser,
-  report,
-  upstream,
-  fileGroupName,
-  command,
-  verb,
-  ..._rest
+  actionDurationMs: _actionDurationMs,
+  ...rest
 }: LintAction): LintAction => ({
-  paths,
-  linter,
-  parser,
-  report,
-  upstream,
-  fileGroupName,
-  command,
-  verb,
+  ...rest,
 });
 
-const extractTaskFailureFields = ({ name, message, ..._rest }: TaskFailure): TaskFailure => ({
-  name,
-  message,
+const extractTaskFailureFields = ({
+  detailPath: _detailPath,
+  ...rest
+}: TaskFailure): TaskFailure => ({
+  ...rest,
 });
 
 /**
  * Remove unwanted fields. Prefer object destructuring to be explicit about required fields
  * for forward compatibility.
  */
-const extractLandingStateFields = (
-  { issues = [], unformattedFiles = [], lintActions = [], taskFailures = [] }: LandingState,
-  skip_message: boolean
-) =>
+const extractLandingStateFields = ({
+  issues = [],
+  unformattedFiles = [],
+  lintActions = [],
+  taskFailures = [],
+}: LandingState) =>
   <LandingState>{
-    issues: sort(issues.map((i) => extractFileIssueFields(i, skip_message))).asc([
+    issues: sort(issues).asc([
       (issue) => issue.file,
       (issue) => issue.line,
       (issue) => issue.column,
       (issue) => issue.code,
       (issue) => issue.message,
     ]),
-    unformattedFiles: sort(
-      unformattedFiles.map((i) => extractFileIssueFields(i, skip_message))
-    ).asc([
+    unformattedFiles: sort(unformattedFiles).asc([
       (issue) => issue.file,
       (issue) => issue.line,
       (issue) => issue.column,
@@ -97,12 +56,12 @@ const extractLandingStateFields = (
   };
 
 /**
- * Extract the LandingState from an input `json`, only retrieving assertable fields. Discard
- * fields that depend on git and cache states. Also sorts repeatable fields deterministically.
+ * Extract the LandingState from an input `json`, discarding time-based fields.
+ * Also sorts repeatable fields deterministically.
  * @param json The nonempty `outputJson` from a `TrunkRunResult`
  */
-export const extractLandingState = (json: unknown, skip_message = false): LandingState =>
-  extractLandingStateFields(json as LandingState, skip_message);
+export const extractLandingState = (json: unknown): LandingState =>
+  extractLandingStateFields(json as LandingState);
 
 /**
  * Attempt to parse the JSON result of a `trunk check` or `trunk fmt` run into
