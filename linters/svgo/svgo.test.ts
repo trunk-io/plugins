c47e32e8007efb8d03610af398d7151c91eb1812
Tyler Jang <tyler@trunk.io>
2023-01-10 23:42:43 -0800

    make svgo and oxipng formatters

--- linters/svgo/svgo.test.ts
+++ linters/svgo/svgo.test.ts
@@ -1,11 +1,3 @@
-import path from "path";
-import { customLinterCheckTest } from "tests";
-import { TEST_DATA } from "tests/utils";
+import { linterFmtTest } from "tests";
 
-// As mentioned in the plugin.yaml, svgo could be a formatter, but is not classified as such.
-// Thus, rather than having just a diagnostic telling us the file is unformatted, we run with `check -y` to assert its formats.
-customLinterCheckTest({
-  linterName: "svgo",
-  args: "-y",
-  pathsToSnapshot: [path.join(TEST_DATA, "basic.svg")],
-});
+linterFmtTest({ linterName: "svgo" });
