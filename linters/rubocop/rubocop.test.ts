099bf60acb9b6fad4a8f6777a0f93e253cacff83
Tyler Jang <tyler@trunk.io>
2023-01-13 14:22:53 -0800

    make rubocop linux-only

--- linters/rubocop/rubocop.test.ts
+++ linters/rubocop/rubocop.test.ts
@@ -1,4 +1,17 @@
-import { linterCheckTest, linterFmtTest } from "tests";
+import path from "path";
+import { customLinterCheckTest, customLinterFmtTest } from "tests";
+import { TEST_DATA } from "tests/utils";
 
-linterCheckTest({ linterName: "rubocop" });
-linterFmtTest({ linterName: "rubocop" });
+// Ruby build is quite slow on Mac, so only run tests on linux for now
+customLinterCheckTest({
+  linterName: "rubocop",
+  testName: "basic",
+  args: "-a",
+  exclusiveOS: ["linux"],
+});
+customLinterFmtTest({
+  linterName: "rubocop",
+  args: "-a",
+  pathsToSnapshot: [path.join(TEST_DATA, "basic.rb")],
+  exclusiveOS: ["linux"],
+});
