d243f54b14c48b9183cacd870389fe14e25bbdf9
Tyler Jang <tyler@trunk.io>
2023-01-13 14:28:03 -0800

    make brakeman linux-only

--- linters/brakeman/brakeman.test.ts
+++ linters/brakeman/brakeman.test.ts
@@ -2,5 +2,5 @@ import { customLinterCheckTest } from "tests";
 import { osTimeoutMultiplier, TEST_DATA } from "tests/utils";
 
 // Note that the ruby setup can sometimes take a while.
-jest.setTimeout(600000 * osTimeoutMultiplier); // 600s or 1200s
-customLinterCheckTest({ linterName: "brakeman", args: TEST_DATA });
+jest.setTimeout(300000 * osTimeoutMultiplier); // 300s or 900s
+customLinterCheckTest({ linterName: "brakeman", args: TEST_DATA, exclusiveOS: ["linux"] });
--- linters/rufo/rufo.test.ts
+++ linters/rufo/rufo.test.ts
@@ -3,7 +3,7 @@ import { customLinterCheckTest, customLinterFmtTest } from "tests";
 import { osTimeoutMultiplier, TEST_DATA } from "tests/utils";
 
 // Note that the first-time ruby/rufo download can sometimes take a while.
-jest.setTimeout(300000 * osTimeoutMultiplier); // 300s or 600s
+jest.setTimeout(300000 * osTimeoutMultiplier); // 300s or 900s
 
 // Rufo succeeds on empty files
 customLinterCheckTest({
