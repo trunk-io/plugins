f0b7dabbb084c183249b70f0c15b766d6fe6893b
Tyler Jang <tyler@trunk.io>
2023-01-13 12:37:54 -0800

    increase timeouts

--- linters/brakeman/brakeman.test.ts
+++ linters/brakeman/brakeman.test.ts
@@ -1,4 +1,6 @@
 import { customLinterCheckTest } from "tests";
-import { TEST_DATA } from "tests/utils";
+import { osTimeoutMultiplier, TEST_DATA } from "tests/utils";
 
+// Note that the ruby setup can sometimes take a while.
+jest.setTimeout(600000 * osTimeoutMultiplier); // 600s or 1200s
 customLinterCheckTest({ linterName: "brakeman", args: TEST_DATA });
--- linters/rufo/rufo.test.ts
+++ linters/rufo/rufo.test.ts
@@ -2,7 +2,7 @@ import { linterCheckTest, linterFmtTest } from "tests";
 import { osTimeoutMultiplier } from "tests/utils";
 
 // Note that the first-time ruby/rufo download can sometimes take a while.
-jest.setTimeout(300000 * osTimeoutMultiplier); // 300s or 600s
+jest.setTimeout(600000 * osTimeoutMultiplier); // 600s or 1200s
 
 // Rufo succeeds on empty files
 linterCheckTest({ linterName: "rufo", namedTestPrefixes: ["empty"] });
