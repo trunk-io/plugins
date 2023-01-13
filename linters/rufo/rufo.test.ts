8bf7fc5efd385f24b56c093fc7fe84ef4a440955
Tyler Jang <tyler@trunk.io>
2023-01-13 14:26:16 -0800

    make rufo linux-only

--- linters/rufo/rufo.test.ts
+++ linters/rufo/rufo.test.ts
@@ -1,10 +1,21 @@
-import { linterCheckTest, linterFmtTest } from "tests";
-import { osTimeoutMultiplier } from "tests/utils";
+import path from "path";
+import { customLinterCheckTest, customLinterFmtTest } from "tests";
+import { osTimeoutMultiplier, TEST_DATA } from "tests/utils";
 
 // Note that the first-time ruby/rufo download can sometimes take a while.
-jest.setTimeout(600000 * osTimeoutMultiplier); // 600s or 1200s
+jest.setTimeout(300000 * osTimeoutMultiplier); // 300s or 600s
 
 // Rufo succeeds on empty files
-linterCheckTest({ linterName: "rufo", namedTestPrefixes: ["empty"] });
+customLinterCheckTest({
+  linterName: "rufo",
+  testName: "empty",
+  args: "-a",
+  exclusiveOS: ["linux"],
+});
 
-linterFmtTest({ linterName: "rufo", namedTestPrefixes: ["basic"] });
+customLinterFmtTest({
+  linterName: "rufo",
+  args: "-a",
+  pathsToSnapshot: [path.join(TEST_DATA, "basic.rb")],
+  exclusiveOS: ["linux"],
+});
