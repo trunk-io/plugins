76376e12e3f1fda1ab25c553ecd972da681c75e7
Tyler Jang <tyler@trunk.io>
2023-01-13 17:51:18 -0800

    added taplo

---
+++ linters/taplo/taplo.test.ts
@@ -0,0 +1,19 @@
+import { customLinterCheckTest, customLinterFmtTest } from "tests";
+import semver from "semver";
+import path from "path";
+import { TEST_DATA } from "tests/utils";
+
+// taplo doesn't use semver versioning
+// Examples of taplo versions include release-cli-0.6.0, release-taplo-cli-0.7.0, 0.8.0
+const versionGreaterThan = (a: string, b: string) => {
+  const normalizedA = a.replace("release-cli-", "").replace("release-taplo-cli-", "");
+  const normalizedB = b.replace("release-cli-", "").replace("release-taplo-cli-", "");
+  return semver.gt(normalizedA, normalizedB);
+};
+
+customLinterCheckTest({
+  linterName: "taplo",
+  args: "-a -y",
+  pathsToSnapshot: [path.join(TEST_DATA, "basic.toml")],
+  versionGreaterThan,
+});
