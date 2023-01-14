7a9e17598d3f279cb1e94d07bfb782d3a3d7020d
Tyler Jang <tyler@trunk.io>
2023-01-13 16:03:21 -0800

    fix gitleaks

--- linters/gitleaks/gitleaks.test.ts
+++ linters/gitleaks/gitleaks.test.ts
@@ -3,7 +3,8 @@ import { customLinterCheckTest } from "tests";
 import { TEST_DATA } from "tests/utils";
 
 // gitleaks version 7.0.0 only had a darwin amd64 release asset
-const checkLinterVersionForInclusion = (version: string) => version != "7.0.0";
+const checkLinterVersionForInclusion = (version: string) =>
+  version != "7.0.0" || process.platform != "darwin";
 
 customLinterCheckTest({
   linterName: "gitleaks",
