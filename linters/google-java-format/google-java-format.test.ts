58acbd86e0a62f18405ea58e8a8c472175b4c8cf
Sam Lijin <sam@trunk.io>
2023-01-18 13:00:04 -0800

    fix now that jdks are merged

--- linters/google-java-format/google-java-format.test.ts
+++ linters/google-java-format/google-java-format.test.ts
@@ -1,5 +1,3 @@
 import { linterFmtTest } from "tests";
 
-// TODO(Tyler): Add .clang-format file from configs
-// TODO(Tyler): We will eventually need to add a couple more test cases involving failure modes and other autofixes.
 linterFmtTest({ linterName: "google-java-format" });
