801ac1de83fc6b7908ea5ea0782dcecd93522d6f
Tyler Jang <tyler@trunk.io>
2023-01-30 17:04:45 -0800

    Added additional pylint test coverage (#98)
    pylint has some additional levels that only appear when corresponding
    rules are enabled in `.pylintrc`. This PR adds an additional test for
    coverage that these levels are parsed correctly.
    
    This PR passing CI and merging is blocked by:
    - Updating the root .trunk/trunk.yaml version to a new release that
    includes the fix for parsing pylint levels
    
    A quick overview of the investigation here:
    1. I had to rebase off of #93, since it has some small improvements to
    debugging and environment variables.
    2. Added an additional test to `linters/pylint/pylint.test.ts` with a
    preCheck that moves the `.pylintrc` file to the root of the sandbox
    workspace. This `.pylintrc` enabled rule I0010, which was triggered by
    new lines added to `severity.py`.
    3. Ran `npm test linters/pylint`. A new snapshot was dumped with a
    linter failure.
    4. Ran `PLUGINS_TEST_CLI_PATH=<path/to/fixed/binary>
    PLUGINS_TEST_LINTER_VERSION=KnownGoodVersion npm test linters/pylint`
    and observed that file issues were correctly reported, with no linter
    failures.
    5. Ran `PLUGINS_TEST_CLI_PATH=<path/to/fixed/binary>
    PLUGINS_TEST_LINTER_VERSION=KnownGoodVersion
    PLUGINS_TEST_UPDATE_SNAPSHOTS=true npm test linters/pylint` to replace
    the existing snapshot.
    6. Ran `PLUGINS_TEST_CLI_PATH=<path/to/fixed/binary>
    PLUGINS_TEST_LINTER_VERSION=Latest npm test linters/pylint` to verify
    that the diagnostics did not change with the latest (or any in-between)
    version of pylint.
    7. I had the option of trimming the scope of the pylint config test to
    run on an explicit path over `-a`, but I figured the coverage with an
    additional config would be nice to have.
    8. Additional consultation of the
    [docs](https://pylint.readthedocs.io/en/v2.14.5/user_guide/configuration/all-options.html#evaluation)
    revealed that pylint has had these levels for a while.

--- linters/pylint/pylint.test.ts
+++ linters/pylint/pylint.test.ts
@@ -1,3 +1,13 @@
-import { linterCheckTest } from "tests";
+import path from "path";
+import { customLinterCheckTest, linterCheckTest, TestCallback } from "tests";
+import { TEST_DATA } from "tests/utils";
 
 linterCheckTest({ linterName: "pylint" });
+
+// pylint "info" and "fatal" severities only show up when certain rules are enabled in .pylintrc.
+// To test that these levels are appropriately parsed, use the following test setup.
+const preCheck: TestCallback = (driver) => {
+  driver.moveFile(path.join(TEST_DATA, ".pylintrc"), ".pylintrc");
+};
+
+customLinterCheckTest({ linterName: "pylint", testName: "config", args: "-a", preCheck });
