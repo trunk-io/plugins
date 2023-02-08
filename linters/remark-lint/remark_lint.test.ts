9131f963110fc90a9a96deb0538925b3599e0b10
Tyler Jang <tyler@trunk.io>
2023-02-07 16:02:45 -0800

    Fix remark-lint tests (#150)
    The last commit broke some of the tests. Namely, the no-tabs plugin was
    removed, the subcommands were renamed, and I believe the original file
    was saved with spaces.

--- linters/remark-lint/remark_lint.test.ts
+++ linters/remark-lint/remark_lint.test.ts
@@ -1,5 +1,12 @@
-import { linterCheckTest, linterFmtTest } from "tests";
+import path from "path";
+import { customLinterCheckTest, linterFmtTest } from "tests";
+import { skipOS, TEST_DATA } from "tests/utils";
 
 linterFmtTest({ linterName: "remark-lint" });
 
-linterCheckTest({ linterName: "remark-lint" });
+// TODO(Tyler): Fix custom parsers sometimes breaking on Mac
+customLinterCheckTest({
+  linterName: "remark-lint",
+  args: path.join(TEST_DATA, "basic.in.md"),
+  skipTestIf: skipOS(["darwin"]),
+});
--- linters/renovate/renovate.test.ts
+++ linters/renovate/renovate.test.ts
@@ -1,6 +1,7 @@
 import { customLinterCheckTest } from "tests";
 import { skipOS } from "tests/utils";
 
+// TODO(Tyler): Fix custom parsers sometimes breaking on Mac
 // python missing on mac
 customLinterCheckTest({
   linterName: "renovate",
