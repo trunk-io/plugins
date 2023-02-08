9245dc9a138408f656400d50c332a88b2be7acef
Tyler Jang <tyler@trunk.io>
2023-02-08 14:11:11 -0800

    upgrade trunk, bypass validated

--- tests/driver/index.ts
+++ tests/driver/index.ts
@@ -193,7 +193,7 @@ export class TrunkDriver {
       const linterVersionString = `${this.linter}${versionString}`;
       // Prefer calling `check enable` over editing trunk.yaml directly because it also handles version, etc.
       this.debug("Enabling %s", linterVersionString);
-      await this.run(`check enable ${linterVersionString} --monitor=false`);
+      await this.run(`check enable ${linterVersionString} --monitor=false --bypass-validated`);
 
       // Retrieve the enabled version
       const newTrunkContents = fs.readFileSync(
