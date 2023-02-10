404b7539af8a0bc778923a20f1e0ffd07353aade
Tyler Jang <tyler@trunk.io>
2023-02-09 17:22:41 -0800

    add tests for mac

--- linters/renovate/renovate.test.ts
+++ linters/renovate/renovate.test.ts
@@ -1,10 +1,6 @@
 import { customLinterCheckTest } from "tests";
-import { skipOS } from "tests/utils";
 
-// TODO(Tyler): Fix custom parsers sometimes breaking on Mac
-// python missing on mac
 customLinterCheckTest({
   linterName: "renovate",
   args: "-a",
-  skipTestIf: skipOS(["darwin"]),
 });
