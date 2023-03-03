58545b46ab1bf84fb19deb3af017a1ea3a15634c
Tyler Jang <tyler@trunk.io>
2023-03-03 15:23:07 -0800

    addressed ansible-lint regression

--- linters/ansible-lint/ansible_lint.test.ts
+++ linters/ansible-lint/ansible_lint.test.ts
@@ -4,10 +4,12 @@ import { TrunkDriver } from "tests/driver";
 import { TEST_DATA } from "tests/utils";
 
 // ansible-lint's trunk invokations are trigger-based, so we need to add this to the trunk.yaml.
-const preCheck = (driver: TrunkDriver) => {
-  const trunkYamlPath = ".trunk/trunk.yaml";
-  const currentContents = driver.readFile(trunkYamlPath);
-  const newContents = currentContents.concat(`  triggers:
+const preCheck =
+  (fqcn = true) =>
+  (driver: TrunkDriver) => {
+    const trunkYamlPath = ".trunk/trunk.yaml";
+    const currentContents = driver.readFile(trunkYamlPath);
+    const newContents = currentContents.concat(`  triggers:
   # Run these linters
     - linters:
         - ansible-lint
@@ -18,9 +20,23 @@ const preCheck = (driver: TrunkDriver) => {
       targets:
         - jboss-standalone
 `);
-  driver.writeFile(trunkYamlPath, newContents);
+    driver.writeFile(trunkYamlPath, newContents);
 
-  driver.moveFile(path.join(TEST_DATA, "jboss-standalone"), "jboss-standalone");
-};
+    driver.moveFile(path.join(TEST_DATA, "jboss-standalone"), "jboss-standalone");
 
-customLinterCheckTest({ linterName: "ansible-lint", preCheck });
+    // See linters/ansible-lint/readme.md for details on non-FQCN support
+    if (!fqcn) {
+      const configFile = "jboss-standalone/demo-aws-launch.yml";
+      driver.writeFile(
+        configFile,
+        driver.readFile(configFile).replace("amazon.aws.ec2_instance", "ec2")
+      );
+    }
+  };
+
+customLinterCheckTest({ linterName: "ansible-lint", testName: "FQCN", preCheck: preCheck() });
+customLinterCheckTest({
+  linterName: "ansible-lint",
+  testName: "non_FQCN",
+  preCheck: preCheck(false),
+});
--- linters/ansible-lint/readme.md
+++ linters/ansible-lint/readme.md
@@ -1,5 +1,7 @@
 # Ansible-Lint
 
+## Usage
+
 [Ansible-lint](https://github.com/ansible/ansible-lint) is used to check ansible playbooks. In order
 to integrate well with trunk, ansible is usually run using triggers. The trigger system allows file
 changes to trigger lint runs. An example of an ansible-lint trigger is included below, but more
@@ -20,4 +22,20 @@ lint:
       # On this target (A directory in this case)
       targets:
         - test_data/jboss-standalone
+```
+
+## FQCN
+
+As of version `6.12.0`, ansible-lint began requiring
+[fully-qualified collection names](https://ansible-lint.readthedocs.io/rules/fqcn/) and canonical
+module names in some contexts. Violation of this pattern can result in fatal syntax warning
+diagnostics. If you're using an older version of ansible that still supports short-form or aliased
+names, you can add the following to your trunk.yaml:
+
+```yaml
+lint:
+  enabled:
+    - ansible-lint@x.y.z:
+        packages:
+          - ansible-core@2.13.7
 ```
