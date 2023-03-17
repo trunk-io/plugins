97092316bcf1f8d33a8e0bf9f26bdfa866cc4e57
Tyler Jang <tyler@trunk.io>
2023-03-16 17:19:28 -0700

    add default config

--- linters/cspell/cspell.test.ts
+++ linters/cspell/cspell.test.ts
@@ -1,52 +1,61 @@
-import { customLinterCheckTest, TestCallback } from "tests";
+import { customLinterCheckTest } from "tests";
+import { TrunkDriver } from "tests/driver";
 import { skipOS } from "tests/utils";
+
+const preCheck = (addDictionary: boolean) => (driver: TrunkDriver) => {
+  const defaultTestingConfig = `$schema: https://raw.githubusercontent.com/streetsidesoftware/cspell/main/cspell.schema.json
+version: "0.2"
+suggestionsTimeout: 5000`;
+
+  const dictionaryConfig = `
+dictionaryDefinitions:
+  - name: plugin-words
+    path: cspell-words.txt
+    addWords: true
+dictionaries:
+  - plugin-words`;
+
+  // Create config
+  driver.writeFile(
+    "cspell.config.yaml",
+    addDictionary ? defaultTestingConfig.concat(dictionaryConfig) : defaultTestingConfig
+  );
+
+  if (addDictionary) {
+    // Create dictionary
+    driver.writeFile(
+      "cspell-words.txt",
+      `cachedir
+codespell
+commitlint
+eamodio
+elif
+kwargs
+proto
+protobuf
+pyyaml
+runtimes
+sarif
+SCRIPTDIR
+trunkio
+vuln`
+    );
+  }
+};
 
 // TODO(Tyler): Determine root cause of flakiness on Mac runners
 customLinterCheckTest({
   linterName: "cspell",
   testName: "basic",
   args: "-a",
+  preCheck: preCheck(false),
   skipTestIf: skipOS(["darwin"]),
 });
-
-const preCheck: TestCallback = (driver) => {
-  // Create config
-  driver.writeFile(
-    "cspell.config.yaml",
-    `$schema: https://raw.githubusercontent.com/streetsidesoftware/cspell/main/cspell.schema.json
-version: "0.2"
-dictionaryDefinitions:
-  - name: plugin-words
-    path: cspell-words.txt
-    addWords: true
-dictionaries:
-  - plugin-words`
-  );
-
-  // Create dictionary
-  driver.writeFile(
-    "cspell-words.txt",
-    `cachedir
-  codespell
-  commitlint
-  eamodio
-  elif
-  kwargs
-  proto
-  protobuf
-  pyyaml
-  runtimes
-  sarif
-  SCRIPTDIR
-  trunkio
-  vuln`
-  );
-};
 
 customLinterCheckTest({
   linterName: "cspell",
   testName: "dictionary",
   args: "-a",
-  preCheck,
+  preCheck: preCheck(true),
   skipTestIf: skipOS(["darwin"]),
 });
