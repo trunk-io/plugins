2c5e28b44c4fea6e52cd93550ebf7e68d6f884a1
Tyler Jang <tyler@trunk.io>
2023-04-11 13:15:48 -0700

    fix tests with upgrade

--- linters/prettier/prettier.test.ts
+++ linters/prettier/prettier.test.ts
@@ -3,7 +3,11 @@ import { TrunkDriver } from "tests/driver";
 
 // Grab the root .prettierrc.yaml
 const preCheck = (driver: TrunkDriver) => {
-  driver.copyFileFromRoot(".trunk/configs/.prettierrc.yaml");
+  driver.writeFile(
+    ".trunk/configs/.prettierrc.yaml",
+    `printWidth: 100
+proseWrap: always`
+  );
 };
 
 // TODO(Tyler): We will eventually need to add a couple more test cases involving other file types.
