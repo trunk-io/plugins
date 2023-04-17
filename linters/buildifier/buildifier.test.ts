9abccecf7e4dad3eb30955c6403a399353452035
Sam Lijin <sam@trunk.io>
2023-04-16 19:06:55 -0700

    feat: recognize .buildifier.json
    Buildifier configuration is very poorly documented: source code comments
    provide some pointers to how this works, but for the most part you have
    to actually read the source code to figure out how to feed configuration
    in.
    
    The semantics are this:
    
      * a `.buildifier.json` at the root of the workspace will be read into
        the configuration for a buildifier invocation
        ([link](https://github.com/bazelbuild/buildtools/blob/6.1.0/buildifier/buildifier.go#L85))
    
      * `tables` specifies a path to a tables config that _overwrites_ the
        default tables config
    
      * `add_tables` specifies a path to a tables config that is merged
        into the default tables config
    
      * tables configs allow overriding buildifier's behavior with respect
        to a given attribute
        ([link](https://github.com/bazelbuild/buildtools/blob/6.1.0/tables/tables.go));
        for example, adding an attribute name `fizz` to `IsSortableListArg` tells
        buildifier that all `fizz` attributes for all rules should be sorted
    
    Note that buildifier JSON parsing is lenient: unrecognized fields do
    _not_ throw an error (this is how the "human documentation" fields
    work).

--- linters/buildifier/buildifier.test.ts
+++ linters/buildifier/buildifier.test.ts
@@ -1,5 +1,32 @@
-import { linterCheckTest, linterFmtTest } from "tests";
+import path from "path";
+import { customLinterFmtTest, linterCheckTest } from "tests";
+import { TrunkDriver } from "tests/driver";
+import { TEST_DATA } from "tests/utils";
+
+const preFmt = (driver: TrunkDriver) => {
+  const buildifierTablesContents = `
+{
+  "IsLabelArg": {},
+  "LabelDenylist": {},
+  "IsSortableListArg": {
+    "fizz": true
+  },
+  "SortableAllowlist": {},
+  "NamePriority": {}
+}
+  `;
+
+  driver.moveFile(".buildifier.json", ".trunk/configs/.buildifier.json");
+  driver.deleteFile(".buildifier-tables.json");
+  driver.writeFile(".trunk/configs/.buildifier-tables.json", buildifierTablesContents);
+};
 
 // TODO(Tyler): We will eventually need to add a couple more test cases involving failure modes.
 linterCheckTest({ linterName: "buildifier" });
-linterFmtTest({ linterName: "buildifier" });
+customLinterFmtTest({
+  linterName: "buildifier",
+  args: "-a",
+  testName: "add_tables",
+  pathsToSnapshot: [path.join(TEST_DATA, "add_tables.in.BUILD")],
+  preCheck: preFmt,
+});
