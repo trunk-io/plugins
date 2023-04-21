86d84b94dc873da41d66df3d7ef76a35876fd645
Felix Sargent <fsargent@users.noreply.github.com>
2023-04-20 17:51:15 -0700

    Initial commit of sourcery

---
+++ linters/sourcery/sourcery.test.ts
@@ -0,0 +1,8 @@
+import { customLinterCheckTest, linterCheckTest } from "tests";
+
+linterCheckTest({ linterName: "sourcey" });
+
+// sourcer "info" and "fatal" severities only show up when certain rules are enabled in .sourceryrc.
+// To test that these levels are appropriately parsed, use the following test setup.
+
+customLinterCheckTest({ linterName: "sourcery", testName: "config" });
