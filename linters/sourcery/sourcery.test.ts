663ddbbab5d82dd0b7657193fff67943e23c8417
Felix Sargent <fsargent@users.noreply.github.com>
2023-04-21 09:21:22 -0700

    Add snapshot of test. Add in_place:true

--- linters/sourcery/sourcery.test.ts
+++ linters/sourcery/sourcery.test.ts
@@ -1,6 +1,6 @@
 import { customLinterCheckTest, linterCheckTest } from "tests";
 
-linterCheckTest({ linterName: "sourcey" });
+linterCheckTest({ linterName: "sourcery" });
 
 // sourcer "info" and "fatal" severities only show up when certain rules are enabled in .sourceryrc.
 // To test that these levels are appropriately parsed, use the following test setup.
