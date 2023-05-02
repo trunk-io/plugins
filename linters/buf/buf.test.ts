9ae05c23f4e13c04337f4f3b1fe0023a7d4df57c
Tyler Jang <tyler@trunk.io>
2023-05-01 17:04:48 -0700

    include suggest_if and fix* buf breaking

--- linters/buf/buf.test.ts
+++ linters/buf/buf.test.ts
@@ -1,34 +1,53 @@
-// import * as path from "path";
-import { linterCheckTest, linterFmtTest } from "tests";
-// import { TrunkLintDriver } from "tests/driver";
-// import { TEST_DATA } from "tests/utils";
+import * as path from "path";
+import { customLinterCheckTest, linterCheckTest, linterFmtTest } from "tests";
+import { TrunkLintDriver } from "tests/driver";
+import { TEST_DATA } from "tests/utils";
 
 // buf-breaking detects breaking changes to proto files by using git-awareness
-// const preCheck = async (driver: TrunkLintDriver) => {
-//   if (driver.gitDriver) {
-//     const inputName = "buf_breaking.in.proto";
-//     const inputPath = path.join(TEST_DATA, inputName);
+const preCheck = (addDup: boolean) => async (driver: TrunkLintDriver) => {
+  if (driver.gitDriver) {
+    const inputName = "buf_breaking.in.proto";
+    const inputPath = path.join(TEST_DATA, inputName);
 
-//     const newContents = `
-//     syntax = "proto3";
+    const dupInputName = "buf_breaking_dup.in.proto";
+    const dupInputPath = path.join(TEST_DATA, dupInputName);
 
-//     package trunk;
+    const newContents = `
+    syntax = "proto3";
 
-//     // Change the types of these fields (breaking change)
-//     message HelloWorld {
-//       int32 hello = 1;
-//       int32 world = 2;
-//     }
-//     `;
+    package trunk;
 
-//     await driver.gitDriver.add(inputPath).commit("Committed original version");
-//     driver.writeFile(inputPath, newContents);
-//   }
-// };
+    // Change the types of these fields (breaking change)
+    message HelloWorld {
+      int32 hello = 1;
+      int32 world = 3;
+    }
+    `;
+
+    await driver.gitDriver.add(inputPath).commit("Committed original version");
+    driver.writeFile(inputPath, newContents);
+
+    if (addDup) {
+      driver.writeFile(dupInputPath, newContents);
+    }
+  }
+};
 
 // TODO(Tyler): We will eventually need to add a couple more test cases involving failure modes and more coverage for format.
 linterCheckTest({ linterName: "buf-lint", namedTestPrefixes: ["buf_lint"] });
 linterFmtTest({ linterName: "buf-format", namedTestPrefixes: ["buf_lint"] });
 
-// TODO(Tyler): Buf-breaking tests and the config itself are currently in a semi-broken state. Need to fix and re-enable.
-// linterCheckTest({ linterName: "buf-breaking", namedTestPrefixes: ["buf_breaking"], preCheck });
+// NOTE(Tyler): This isn't a perfect config of buf breaking. For instance, if files are moved/renamed, buf cannot account for
+// them, but it offers a best effort that covers primary buf use cases.
+customLinterCheckTest({
+  linterName: "buf-breaking",
+  testName: "basic",
+  args: "-a",
+  preCheck: preCheck(false),
+});
+customLinterCheckTest({
+  linterName: "buf-breaking",
+  testName: "dupFile",
+  args: "-a",
+  preCheck: preCheck(true),
+});
