921f96c4f1f862e35922d6d0fae63587bb6bc0d9
Patrice Boisclair-Laberge <pat@trunk.io>
2023-05-31 01:48:05 +0000

    Fix tests

--- linters/txtpbfmt/txtpbfmt.test.ts
+++ linters/txtpbfmt/txtpbfmt.test.ts
@@ -1,15 +1,3 @@
-import path from "path";
-import { customLinterCheckTest } from "tests";
-import { TEST_DATA } from "tests/utils";
+import { linterFmtTest } from "tests";
 
-customLinterCheckTest({
-  linterName: "txtpbfmt",
-  testName: "basic_check",
-});
-
-customLinterCheckTest({
-  linterName: "txtpbfmt",
-  testName: "basic_lint_test",
-  pathsToSnapshot: [path.join(TEST_DATA, "test0.textproto"), path.join(TEST_DATA, "test1.textpb")],
-  versionGreaterThanOrEqual: (_a: string, _b: string) => true,
-});
+linterFmtTest({ linterName: "txtpbfmt", namedTestPrefixes: ["test0", "test1"] });
