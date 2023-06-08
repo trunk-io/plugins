9429faed1cc858c0af846bb16d66d2378ce94d32
Tyler Jang <tyler@trunk.io>
2023-06-08 14:06:35 -0700

    add setup-ci and poke eslint

--- linters/osv-scanner/osv_scanner.test.ts
+++ linters/osv-scanner/osv_scanner.test.ts
@@ -1,5 +1,6 @@
 import { customLinterCheckTest } from "tests";
 
+// TODO(Tyler): This test is known to be flaky. We need to add another test type for it.
 customLinterCheckTest({
   linterName: "osv-scanner",
   args: "-a -y",
