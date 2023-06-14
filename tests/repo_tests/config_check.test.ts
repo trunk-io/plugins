a600291d7c20dab64aa3d466f98b7e81aa23b307
Tyler Jang <tyler@trunk.io>
2023-06-14 15:31:39 -0700

    bump cli version

--- tests/repo_tests/config_check.test.ts
+++ tests/repo_tests/config_check.test.ts
@@ -23,7 +23,7 @@ describe("Global config health check", () => {
     setupTrunk: true,
     // NOTE: This version should be kept compatible in lockstep with the `required_trunk_version` in plugin.yaml
     // IfChange
-    trunkVersion: "1.10.1-beta.5",
+    trunkVersion: "1.10.1-beta.35",
     // ThenChange plugin.yaml
   });
 
