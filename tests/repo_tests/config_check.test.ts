d11e831234c6b3332c89be164084eda5d43a2020
Patrice Boisclair-Laberge <pat@trunk.io>
2023-06-29 18:08:00 +0000

    Bump required version to support tools in linters

--- tests/repo_tests/config_check.test.ts
+++ tests/repo_tests/config_check.test.ts
@@ -23,7 +23,7 @@ describe("Global config health check", () => {
     setupTrunk: true,
     // NOTE: This version should be kept compatible in lockstep with the `required_trunk_version` in plugin.yaml
     // IfChange
-    trunkVersion: "1.11.2-beta.18",
+    trunkVersion: "1.11.2-beta.19",
     // ThenChange plugin.yaml
   });
 
