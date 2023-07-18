e7ffb97ae8b19150e9d689e645b706a22e81cd11
Tyler Jang <tyler@trunk.io>
2023-07-18 13:38:29 -0700

    Remaining Windows plugins fixes (#370)
    Abridged version of #330
    
    Applies supported_platforms for the following:
    - The following linters are disabled on Windows with no immediate plans
    to support: brakeman, haml-lint, nixpkgs-fmt, rubocop, rufo, standardrb,
    taplo, ansible-lint, scalafmt, semgrep, shellcheck, stringslint,
    swiftformat, swiftlint
    - The following linters are disabled on Windows with some intermediate
    plans to support: checkov, clang-format, clang-tidy, iwyu, ktlint, mypy,
    remark-lint, sort-package-json
    
    Also fixes python needing `SYSTEMROOT` set when invoked from powershell.

--- linters/black/black.test.ts
+++ linters/black/black.test.ts
@@ -2,4 +2,3 @@ import { linterFmtTest } from "tests";
 
 // TODO(Tyler): We will eventually need to add a couple more test cases involving failure modes and other autofixes.
 linterFmtTest({ linterName: "black", namedTestPrefixes: ["basic", "basic_nb"] });
-linterFmtTest({ linterName: "black-py", namedTestPrefixes: ["basic"] });
