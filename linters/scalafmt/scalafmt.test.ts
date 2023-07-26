f3a3ee74c1c66ca3b717e1c4f06b7846d415486c
Patrice Boisclair-Laberge <pat@trunk.io>
2023-07-26 23:10:23 +0000

    Fix all tests on linux arm64

--- linters/scalafmt/scalafmt.test.ts
+++ linters/scalafmt/scalafmt.test.ts
@@ -1,6 +1,6 @@
 import { linterCheckTest, linterFmtTest } from "tests";
 import { TrunkLintDriver } from "tests/driver";
-import { skipOS } from "tests/utils";
+import { skipCPUOS, skipOS } from "tests/utils";
 
 // Add a .scalafmt.confg (required to run)
 const preCheck = (driver: TrunkLintDriver) => {
@@ -8,17 +8,25 @@ const preCheck = (driver: TrunkLintDriver) => {
   driver.writeFile(".scalafmt.conf", contents);
 };
 
+const skipTestIfLambda = (version?: string | undefined) => {
+  const os_version = skipOS(["win32"])(version);
+  if (os_version) {
+    return os_version;
+  }
+  return skipCPUOS(["linux"], ["arm64"])(version);
+};
+
 // scalafmt succeeds on empty files
 linterCheckTest({
   linterName: "scalafmt",
   namedTestPrefixes: ["empty"],
   preCheck,
-  skipTestIf: skipOS(["win32"]),
+  skipTestIf: skipTestIfLambda,
 });
 
 linterFmtTest({
   linterName: "scalafmt",
   namedTestPrefixes: ["basic"],
   preCheck,
-  skipTestIf: skipOS(["win32"]),
+  skipTestIf: skipTestIfLambda,
 });
--- tools/gt/gt.test.ts
+++ tools/gt/gt.test.ts
@@ -1,7 +1,9 @@
 import { makeToolTestConfig, toolTest } from "tests";
+import { skipCPUOS } from "tests/utils";
 
 toolTest({
   toolName: "gt",
   toolVersion: "0.20.19",
   testConfigs: [makeToolTestConfig({ command: ["gt", "--version"], expectedOut: "0.20.19" })],
+  skipTestIf: skipCPUOS(["linux"], ["arm64"]),
 });
