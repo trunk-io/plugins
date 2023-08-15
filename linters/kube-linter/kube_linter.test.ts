a4c0c8748bc7dae816d7d13a01592143eda45fa6
Tyler Jang <tyler@trunk.io>
2023-08-15 10:58:40 -0700

    fix tools and bump timeouts

--- linters/kube-linter/kube_linter.test.ts
+++ linters/kube-linter/kube_linter.test.ts
@@ -1,4 +1,7 @@
 import { linterCheckTest } from "tests";
+import { osTimeoutMultiplier } from "tests/utils";
+
+jest.setTimeout(600000 * osTimeoutMultiplier); // 300s or 900s
 
 // TODO(Tyler): We will eventually need to add a couple more test cases involving failure modes.
 linterCheckTest({ linterName: "kube-linter" });
