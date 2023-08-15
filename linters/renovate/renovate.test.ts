45305e5d9d8081e5e77258a531d2140169362a64
Tyler Jang <tyler@trunk.io>
2023-08-15 12:13:53 -0700

    Fix terraform tools on Windows (#437)
    Had to reconfigure the downloads. Also bumped the timeouts for a few
    slow-running tests. Should resolve most of
    [job](https://github.com/trunk-io/plugins/actions/runs/5864943269/job/15900924730)

--- linters/renovate/renovate.test.ts
+++ linters/renovate/renovate.test.ts
@@ -1,4 +1,7 @@
 import { customLinterCheckTest } from "tests";
+import { osTimeoutMultiplier } from "tests/utils";
+
+jest.setTimeout(600000 * osTimeoutMultiplier); // 300s or 900s
 
 customLinterCheckTest({
   linterName: "renovate",
