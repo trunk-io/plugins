a86f500a1548392d96b3bc8dff53c28d2c50e0b3
Maverick Chung <mavchung@gmail.com>
2023-08-17 11:08:02 -0700

    feat: add trivy secret scanning (#440)
    Adds trivy secret scanning - the reason this wasn't showing up before
    was because trivy by default doesn't scan for secrets in files with
    `/test` in the path. Also reworks the trivy tests to have separate tests
    for each subcommand.
    
    ```
    secrets.py:2:0
      2:0  high  AWS Access Key ID       trivy/aws-access-key-id    
      3:0  high  AWS Secret Access Key   trivy/aws-secret-access-key
     11:0  high  Asymmetric Private Key  trivy/private-key          
    ```

--- linters/trivy/trivy.test.ts
+++ linters/trivy/trivy.test.ts
@@ -1,16 +1,59 @@
 import * as fs from "fs";
 import path from "path";
-import { fuzzyLinterCheckTest } from "tests";
+import { customLinterCheckTest, fuzzyLinterCheckTest, TestCallback } from "tests";
+import { TrunkLintDriver } from "tests/driver";
 import { FileIssue } from "tests/types";
+import { TEST_DATA } from "tests/utils";
 import { createFuzzyMatcher } from "tests/utils/landing_state";
 
-// trunk-ignore(eslint/@typescript-eslint/no-unsafe-assignment)
-const expectedFileIssues = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, "expected_issues.json")).toString()
-);
+const callbackGenerator =
+  (command: string, otherPreCheck?: TestCallback): TestCallback =>
+  (driver) => {
+    const trunkYamlPath = ".trunk/trunk.yaml";
+    const currentContents = driver.readFile(trunkYamlPath);
+    const trivyRegex = /- trivy@(.+)\n/;
+    const newContents = currentContents.replace(
+      trivyRegex,
+      `- trivy@$1:\n        commands: [${command}]\n`,
+    );
+    driver.writeFile(trunkYamlPath, newContents);
+    if (otherPreCheck) {
+      otherPreCheck(driver);
+    }
+  };
+
+const configExpectedFileIssues = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "config_expected_issues.json")).toString(),
+) as FileIssue[];
 
 fuzzyLinterCheckTest({
   linterName: "trivy",
-  args: "-a -y",
-  fileIssueAssertionCallback: createFuzzyMatcher(() => expectedFileIssues as FileIssue[], 12),
+  testName: "config",
+  args: "-a",
+  fileIssueAssertionCallback: createFuzzyMatcher(() => configExpectedFileIssues, 13),
+  preCheck: callbackGenerator("config"),
+});
+
+const vulnExpectedFileIssues = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "vuln_expected_issues.json")).toString(),
+) as FileIssue[];
+
+fuzzyLinterCheckTest({
+  linterName: "trivy",
+  testName: "fs-vuln",
+  args: "-a",
+  fileIssueAssertionCallback: createFuzzyMatcher(() => vulnExpectedFileIssues, 44),
+  preCheck: callbackGenerator("fs-vuln"),
+});
+
+// trivy won't scan files with "/test" in the filepath
+const secretPreCheck = (driver: TrunkLintDriver) => {
+  driver.moveFile(path.join(TEST_DATA, "secrets.py"), path.join("other_data", "secrets.py"));
+};
+
+customLinterCheckTest({
+  linterName: "trivy",
+  testName: "fs-secret",
+  args: "-a",
+  preCheck: callbackGenerator("fs-secret", secretPreCheck),
 });
