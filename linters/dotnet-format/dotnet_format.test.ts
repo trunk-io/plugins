635c9f0029a98d587f69a88f7007e4b7dd589ef3
Tyler Jang <tyler@trunk.io>
2023-08-17 12:56:21 -0700

    working for proof of concept

--- linters/dotnet-format/dotnet_format.test.ts
+++ linters/dotnet-format/dotnet_format.test.ts
@@ -1,4 +1,13 @@
-import { linterFmtTest } from "tests";
+import path from "path";
+import { customLinterFmtTest } from "tests";
+import { TEST_DATA } from "tests/utils";
 
 // TODO(Tyler): We will eventually need to add a couple more test cases involving failure modes.
-linterFmtTest({ linterName: "dotnet-format", namedTestPrefixes: ["basic"] });
+customLinterFmtTest({
+  linterName: "dotnet-format",
+  args: "-a",
+  pathsToSnapshot: [
+    path.join(TEST_DATA, "basic/basic.in.cs"),
+    path.join(TEST_DATA, "basic/Program.cs"),
+  ],
+});
