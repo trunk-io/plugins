f0a3d5667a56640d935929fdf068b2ba3e26a63d
Patrice Boisclair-Laberge <pat@trunk.io>
2023-08-18 16:48:50 +0000

    Add salesforce cli as a tool

---
+++ tools/sfdx/sfdx.test.ts
@@ -0,0 +1,12 @@
+import { makeToolTestConfig, toolTest } from "tests";
+
+toolTest({
+  toolName: "sfdx",
+  toolVersion: "2.0.1",
+  testConfigs: [
+    makeToolTestConfig({
+      command: ["sfdx", "--version"],
+      expectedOut: "2.0.1",
+    }),
+  ],
+});
