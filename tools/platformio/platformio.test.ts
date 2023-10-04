bcdc4f3f29f3386225dafc19fc56eb3a416ac83e
Patrice Boisclair-Laberge <pat@trunk.io>
2023-10-04 20:39:54 +0000

    healthcheck

--- tools/platformio/platformio.test.ts
+++ tools/platformio/platformio.test.ts
@@ -1,14 +1,4 @@
-import { makeToolTestConfig, toolInstallTest, toolTest } from "tests";
-toolTest({
-  toolName: "platformio",
-  toolVersion: "6.1.11",
-  testConfigs: [
-    makeToolTestConfig({
-      command: ["pio", "--version"],
-      expectedOut: "PlatformIO Core, version 6.1.11",
-    }),
-  ],
-});
+import { toolInstallTest } from "tests";
 
 toolInstallTest({
   toolName: "platformio",
