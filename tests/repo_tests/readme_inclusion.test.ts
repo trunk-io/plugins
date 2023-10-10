7811691a48b98ab3785b5d8604117561359ee079
Tyler Jang <tyler@trunk.io>
2023-10-10 12:29:13 -0700

    Repo cleanup (#509)
    Handles a number of cleanup tasks
    - Fixes autopep8 issue conditions originating from pinning an
    `extra_packages` field (see #404). This should fix the issue identified
    in https://github.com/trunk-io/plugins/issues/507. I will also fix the
    underlying CLI bug separately.
    - Updates action descriptions and comments to be clearer and more
    standardized (follow-up to #505).
    - Rename all repo readme files to all caps. This helps them stand out
    more to new users.
    - Update the readme instructions to fix broken links and modernize the
    instructions with Trunk Tools and other practices.
    - Run `trunk upgrade`

--- tests/repo_tests/readme_inclusion.test.ts
+++ tests/repo_tests/readme_inclusion.test.ts
@@ -5,23 +5,23 @@ import { REPO_ROOT } from "tests/utils";
 const excludedLinters: string[] = [];
 const abbreviationMapping = new Map<string, string>([["iwyu", "include-what-you-use"]]);
 
-const readmeContents = fs.readFileSync(path.resolve(REPO_ROOT, "readme.md"), { encoding: "utf-8" });
+const readmeContents = fs.readFileSync(path.resolve(REPO_ROOT, "README.md"), { encoding: "utf-8" });
 const readmeTableContents = readmeContents.substring(
   readmeContents.indexOf("### Supported Linters"),
   readmeContents.indexOf("### Supported Trunk Actions"),
 );
 const reducedReadmeContents = readmeTableContents ? readmeTableContents : readmeContents;
 
-// This test asserts that all linters are included in the root readme.md. This does not cover subcommands, and it assumes one
+// This test asserts that all linters are included in the root README.md. This does not cover subcommands, and it assumes one
 // directory per linter. Name mapping can be achieved through `abbreviationMapping`.
-describe("All linters must be included in readme.md", () => {
+describe("All linters must be included in README.md", () => {
   // Find all linter subdirectories
   const linterDir = path.resolve(REPO_ROOT, "linters");
   const linters = fs
     .readdirSync(linterDir)
     .filter((file) => fs.lstatSync(path.resolve(linterDir, file)).isDirectory());
 
-  // Assert that each linter subdirectory is included in the repo's readme.md
+  // Assert that each linter subdirectory is included in the repo's README.md
   linters
     .filter((linter) => !excludedLinters.includes(linter))
     .forEach((linter) => {
