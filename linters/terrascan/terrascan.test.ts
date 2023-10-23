416a890497418f844fd9ac34f87152943b0fd564
Tyler Jang <tyler@trunk.io>
2023-10-23 15:07:10 -0700

    try adding a git remote

--- linters/terrascan/terrascan.test.ts
+++ linters/terrascan/terrascan.test.ts
@@ -1,3 +1,11 @@
 import { linterCheckTest } from "tests";
+import { TrunkLintDriver } from "tests/driver";
 
-linterCheckTest({ linterName: "terrascan" });
+// NOTE(Tyler): Terrascan will sometimes fail due to a git error:
+// "failed to fetch references from git repo. error: 'some refs were not updated'"
+// Add a remote in order to avoid this error.
+const preCheck = async (driver: TrunkLintDriver) => {
+  await driver.gitDriver?.addRemote("origin", driver.getSandbox());
+};
+
+linterCheckTest({ linterName: "terrascan", preCheck });
