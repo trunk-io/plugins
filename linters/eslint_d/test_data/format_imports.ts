80cf72f9f86b1b18bac9682fd0d3be3e91ba02de
Patrice Boisclair-Laberge <pat@trunk.io>
2023-11-06 19:20:19 +0000

    (feat): Add support for eslint_d

---
+++ linters/eslint_d/test_data/format_imports.ts
@@ -0,0 +1,8 @@
+/* eslint "import/first": "error" */
+import { zaphod } from "beetlebrox";
+import { arthur } from "dent";
+
+export interface Marvin {
+  paranoid: boolean;
+}
+import { ford } from "prefect";
