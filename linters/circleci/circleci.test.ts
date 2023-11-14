1ff8a2f356ed629a78b676c35cb3e0b0953b9102
Tyler Jang <tyler@trunk.io>
2023-11-13 23:55:17 -0800

    make tests run for circleci

--- linters/circleci/circleci.test.ts
+++ linters/circleci/circleci.test.ts
@@ -1,14 +1,6 @@
-import semver from "semver";
 import { customLinterCheckTest } from "tests";
-import { skipCPUOS } from "tests/utils";
 
 customLinterCheckTest({
   linterName: "circleci",
   args: "-a",
-  skipTestIf: (version?: string) => {
-    if (version && semver.gte(version, "0.1.28528")) {
-      return true;
-    }
-    return skipCPUOS([{ os: "darwin", cpu: "arm64" }])(version);
-  },
 });
