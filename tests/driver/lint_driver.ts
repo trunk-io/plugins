49544648042ebc7c1219a3607a9d7e9cc27aa369
Tyler Jang <tyler@trunk.io>
2023-11-27 11:12:57 -0800

    add node shim

--- tests/driver/lint_driver.ts
+++ tests/driver/lint_driver.ts
@@ -94,6 +94,10 @@ plugins:
   sources:
   - id: trunk
     local: ${REPO_ROOT}
+tools:
+  runtimes:
+    # Supports eslint test npm install and additional debugging
+    - node
 lint:
   ignore:
     - linters: [ALL]
