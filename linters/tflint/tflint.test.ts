434bcae2109004333889617c8e87c43c864d31a5
Tyler Jang <tyler@trunk.io>
2023-12-05 14:44:59 -0800

    modify tflint test data

--- linters/tflint/tflint.test.ts
+++ linters/tflint/tflint.test.ts
@@ -3,12 +3,6 @@ import { customLinterCheckTest } from "tests";
 import { TrunkLintDriver } from "tests/driver";
 import { TEST_DATA } from "tests/utils";
 
-// NOTE: because of copying to root of repo, paths will not be in test_data/ in the test repo.
-// const preCheck = (driver: TrunkLintDriver) => {
-//   driver.moveFile(path.join(TEST_DATA, ".tflint.hcl"), path.join(".tflint.hcl"));
-//   driver.moveFile(path.join(TEST_DATA, "aws.tf"), path.join("aws.tf"));
-// };
-
 const preCheckBadConfig = (driver: TrunkLintDriver) => {
   driver.moveFile(path.join(TEST_DATA, "bad.tflint.hcl"), path.join(".tflint.hcl"));
   driver.moveFile(path.join(TEST_DATA, "aws.tf"), path.join("aws.tf"));
@@ -16,7 +10,7 @@ const preCheckBadConfig = (driver: TrunkLintDriver) => {
 
 // Running tflint uses calls to GitHub's APIs. If you are concerned about rate limits, disable this test locally.
 // Because of these rate limits, this test is frequently flaky, so it is disabled for now.
-// customLinterCheckTest({ linterName: "tflint", preCheck });
+// customLinterCheckTest({ linterName: "tflint", args: "-a" });
 customLinterCheckTest({
   linterName: "tflint",
   testName: "bad_config",
