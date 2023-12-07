78edc9f37ef3ca4a96c5629c1c04e8b0a7d0e240
Chris Clearwater <chris@trunk.io>
2023-12-07 15:20:30 -0800

    Add support for graphite `>=1.0.0` (#580)

--- tools/gt/gt.test.ts
+++ tools/gt/gt.test.ts
@@ -1,9 +1,16 @@
 import { makeToolTestConfig, toolTest } from "tests";
-import { skipCPUOS } from "tests/utils";
+import { skipCPUOS, skipOS } from "tests/utils";
 
 toolTest({
   toolName: "gt",
   toolVersion: "0.20.19",
   testConfigs: [makeToolTestConfig({ command: ["gt", "--version"], expectedOut: "0.20.19" })],
   skipTestIf: skipCPUOS([{ os: "linux", cpu: "arm64" }]),
+});
+
+toolTest({
+  toolName: "gt",
+  toolVersion: "1.0.7",
+  testConfigs: [makeToolTestConfig({ command: ["gt", "--version"], expectedOut: "1.0.7" })],
+  skipTestIf: skipOS(["win32"]),
 });
