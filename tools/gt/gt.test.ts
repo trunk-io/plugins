a633333a0754c109b101434039bd1a75935753bc
Tyler Jang <tyler@trunk.io>
2023-12-07 16:34:11 -0800

    disable test on ci

--- tools/gt/gt.test.ts
+++ tools/gt/gt.test.ts
@@ -5,7 +5,15 @@ toolTest({
   toolName: "gt",
   toolVersion: "0.20.19",
   testConfigs: [makeToolTestConfig({ command: ["gt", "--version"], expectedOut: "0.20.19" })],
-  skipTestIf: skipCPUOS([{ os: "linux", cpu: "arm64" }]),
+  skipTestIf: (version) => {
+    if (process.env.CI) {
+      console.log("Skipping gt test for CI until we have better parsing logic.");
+      return true;
+    }
+
+    // Unsupported download
+    return skipCPUOS([{ os: "linux", cpu: "arm64" }])(version);
+  },
 });
 
 toolTest({
