7484dfc8be7a31d0489356bd1c6cc282dac1c454
Tyler Jang <tyler@trunk.io>
2024-01-04 11:16:58 -0800

    clang-tidy

--- linters/clang-tidy/clang_tidy.test.ts
+++ linters/clang-tidy/clang_tidy.test.ts
@@ -9,7 +9,9 @@ const preCheck = (copyConfig: boolean) => (driver: TrunkLintDriver) => {
   const currentContents = driver.readFile(trunkYamlPath);
   // Because clang-tidy requires greater build-level awareness for full functionality,
   // This test provides some simple overrides to exercise the basic functionality.
-  const newContents = currentContents.concat(`  definitions:
+  const newContents = currentContents.concat(`  compile_commands_roots:
+  - test_data
+  definitions:
     - name: clang-tidy
       files: [c++-header, c++-source]
       commands:
@@ -21,33 +23,32 @@ const preCheck = (copyConfig: boolean) => (driver: TrunkLintDriver) => {
       include_scanner_type: none`);
   driver.writeFile(trunkYamlPath, newContents);
 
-  // Move files to root
-  // trunk-ignore-begin(semgrep): paths used here are safe
-  fs.readdirSync(path.resolve(driver.getSandbox(), TEST_DATA)).forEach((file) => {
-    if (file !== ".clang-tidy" || copyConfig) {
-      driver.moveFile(path.join(TEST_DATA, file), file);
-    }
-  });
-  // trunk-ignore-end(semgrep)
+  if (copyConfig) {
+    driver.moveFile(path.join(TEST_DATA, ".clang-tidy"), ".clang-tidy");
+  }
 
   // Resolve templates in compile_commands.json
-  const compileContents = driver.readFile("compile_commands.json");
-  const newCompileContents = compileContents.replace(/\$\{EXECUTION_ROOT\}/g, driver.getSandbox());
-  driver.writeFile("compile_commands.json", newCompileContents);
+  const compileCommandsPath = path.join(TEST_DATA, "compile_commands.json");
+  const compileContents = driver.readFile(compileCommandsPath);
+  const newCompileContents = compileContents.replace(
+    /\$\{EXECUTION_ROOT\}/g,
+    path.resolve(driver.getSandbox(), TEST_DATA),
+  );
+  driver.writeFile(compileCommandsPath, newCompileContents);
 };
 
 // Use both configs for a bit more coverage of different config cases.
 customLinterCheckTest({
   linterName: "clang-tidy",
   testName: "default_config",
-  args: "-a --cache=false",
+  args: `${TEST_DATA} --cache=false`,
   preCheck: preCheck(false),
   skipTestIf: skipOS(["win32"]),
 });
 customLinterCheckTest({
   linterName: "clang-tidy",
   testName: "test_config",
-  args: "-a --cache=false",
+  args: `${TEST_DATA} --cache=false`,
   preCheck: preCheck(true),
   skipTestIf: skipOS(["win32"]),
 });
