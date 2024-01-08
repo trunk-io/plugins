642537b8c448cc7dda79db1bd5e9aede2c8a9e8a
Tyler Jang <tyler@trunk.io>
2024-01-08 10:42:23 -0800

    cleanup, use TEST_DATA

--- linters/iwyu/iwyu.test.ts
+++ linters/iwyu/iwyu.test.ts
@@ -1,6 +1,7 @@
+import * as path from "path";
 import { customLinterCheckTest } from "tests";
 import { TrunkLintDriver } from "tests/driver";
-import { skipOS } from "tests/utils";
+import { skipOS, TEST_DATA } from "tests/utils";
 
 // iwyu doesn't use semver versioning, so we need to pass a custom callback.
 // Examples of iwyu versions include 0.10, 0.19
@@ -12,7 +13,7 @@ const preCheck = (driver: TrunkLintDriver) => {
   // Because include-what-you-use requires greater build-level awareness for full functionality, and we can't rely on certain
   // system-installed tools, this test provides some simple overrides to exercise the basic functionality.
   const newContents = currentContents.concat(`  compile_commands_roots:
-    - test_data
+    - ${TEST_DATA}
   definitions:
     - name: include-what-you-use
       commands:
@@ -26,18 +27,19 @@ const preCheck = (driver: TrunkLintDriver) => {
   driver.writeFile(trunkYamlPath, newContents);
 
   // Resolve templates in compile_commands.json
-  const oldCompileContents = driver.readFile("test_data/compile_commands.json");
+  const compileCommandsPath = path.join(TEST_DATA, "compile_commands.json");
+  const oldCompileContents = driver.readFile(compileCommandsPath);
   const newCompileContents = oldCompileContents.replace(
     /\$\{EXECUTION_ROOT\}/g,
-    `${driver.getSandbox()}/test_data`,
+    `${path.resolve(driver.getSandbox(), TEST_DATA)}`,
   );
-  driver.writeFile("test_data/compile_commands.json", newCompileContents);
+  driver.writeFile(compileCommandsPath, newCompileContents);
 };
 
 customLinterCheckTest({
   linterName: "include-what-you-use",
   args: "-a -y",
-  pathsToSnapshot: ["test_data/test.cc"],
+  pathsToSnapshot: [path.join(TEST_DATA, "test.cc")],
   versionGreaterThanOrEqual,
   preCheck,
   skipTestIf: skipOS(["win32"]),
