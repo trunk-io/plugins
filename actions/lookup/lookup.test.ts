fcc111b21ba5cddd936b211244e5bf902e1b7979
Tyler Jang <tyler@trunk.io>
2024-02-06 20:36:06 -0800

    reorder more

--- actions/lookup/lookup.test.ts
+++ actions/lookup/lookup.test.ts
@@ -7,9 +7,9 @@ import { TrunkActionDriver } from "tests/driver";
 const testCallback = async (driver: TrunkActionDriver) => {
   const result = await driver.runAction(".cli.version");
   expect(result.stderr).toBe("");
-  expect(result.exitCode).toBe(0);
   const strippedStdout = result.stdout.replaceAll('"', "");
   expect(semver.parse(strippedStdout)).toBeTruthy();
+  expect(result.exitCode).toBe(0);
 };
 
 actionRunTest({
