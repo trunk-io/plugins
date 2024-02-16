f12c3f4baaffc08914f0b25ede1c844ab99f3306
Chris Clearwater <chris@trunk.io>
2024-02-16 10:38:54 -0800

    Add ruff format command (#664)

--- linters/ruff/ruff.test.ts
+++ linters/ruff/ruff.test.ts
@@ -1,10 +1,26 @@
-import { linterCheckTest } from "tests";
+import { linterCheckTest, linterFmtTest } from "tests";
+import { TrunkLintDriver } from "tests/driver";
 import { skipOS } from "tests/utils";
 
 linterCheckTest({ linterName: "ruff", namedTestPrefixes: ["basic"] });
+
 // ruff-nbqa still runs correctly on Windows, but the diagnostics are slightly different from the assertions.
 linterCheckTest({
   linterName: "ruff-nbqa",
   namedTestPrefixes: ["basic_nb"],
   skipTestIf: skipOS(["win32"]),
 });
+
+// Due to ruff's format subcommand being disabled by default, we need to manually enable it in our test's trunk.yaml.
+const preCheck = (driver: TrunkLintDriver) => {
+  const trunkYamlPath = ".trunk/trunk.yaml";
+  const currentContents = driver.readFile(trunkYamlPath);
+  const sqlfluffRegex = /- ruff@(.+)\n/;
+  const newContents = currentContents.replace(
+    sqlfluffRegex,
+    "- ruff@$1:\n        commands: [format]\n",
+  );
+  driver.writeFile(trunkYamlPath, newContents);
+};
+
+linterFmtTest({ linterName: "ruff", preCheck, namedTestPrefixes: ["format"] });
