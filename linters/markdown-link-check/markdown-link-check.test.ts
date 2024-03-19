e858d9182935e449400f3f1560c7a40ccbd4144d
Tyler Jang <tyler@trunk.io>
2024-03-19 10:24:33 -0700

    cleanup

--- linters/markdown-link-check/markdown-link-check.test.ts
+++ linters/markdown-link-check/markdown-link-check.test.ts
@@ -1,3 +1,11 @@
 import { linterCheckTest } from "tests";
 
-linterCheckTest({ linterName: "markdown-link-check" });
+const manualVersionReplacer = (version: string) => {
+  if (version === "3.12.1") {
+    // As of 3/19, 3.12.1 cannot check for anchors, so we view it as a bad version.
+    return "3.11.2";
+  }
+  return version;
+};
+
+linterCheckTest({ linterName: "markdown-link-check", manualVersionReplacer });
