ef5ed4278a8b58ba80ee68a1ce010a89d6621f7a
Tyler Jang <tyler@trunk.io>
2024-03-19 15:02:02 -0700

    Various nightly cleanups (#702)
    - Cleanup `sparse_checkout`, which is not a valid input, so this was
    never working
    - Add `manualVersionReplacer` for `markdown-link-check`, since `3.12.1`
    has been broken for a while and a fix hasn't released
    - Remove the old download for `gt` since it is no longer a publicly
    accessible asset. We still have the npm package for newer versions
    
    Successful [nightly
    run](https://github.com/trunk-io/plugins/actions/runs/8347542833/job/22847380037)
    off this branch.

--- tools/gt/gt.test.ts
+++ tools/gt/gt.test.ts
@@ -1,20 +1,5 @@
 import { makeToolTestConfig, toolTest } from "tests";
-import { skipCPUOS, skipOS } from "tests/utils";
-
-toolTest({
-  toolName: "gt",
-  toolVersion: "0.20.19",
-  testConfigs: [makeToolTestConfig({ command: ["gt", "--version"], expectedOut: "0.20.19" })],
-  skipTestIf: (version) => {
-    if (process.env.CI) {
-      console.log("Skipping gt test for CI until we have better parsing logic.");
-      return true;
-    }
-
-    // Unsupported download
-    return skipCPUOS([{ os: "linux", cpu: "arm64" }])(version);
-  },
-});
+import { skipOS } from "tests/utils";
 
 toolTest({
   toolName: "gt",
