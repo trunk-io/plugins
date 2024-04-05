34ff924d19f7de3c83a4c17faac2128fca6bce15
Tyler Jang <tyler@trunk.io>
2024-04-05 14:17:03 -0700

    poke ci

--- linters/markdown-link-check/markdown-link-check.test.ts
+++ linters/markdown-link-check/markdown-link-check.test.ts
@@ -2,7 +2,7 @@ import { linterCheckTest } from "tests";
 
 const manualVersionReplacer = (version: string) => {
   if (version === "3.12.1") {
-    // As of 3/19, 3.12.1 cannot check for anchors, so we view it as a bad version.
+    // As of 4/5, 3.12.1 cannot check for anchors, so we view it as a bad version.
     return "3.11.2";
   }
   return version;
