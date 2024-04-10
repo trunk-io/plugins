5611fee80d1d5a639a2a76504c02fbd40653c4e7
Tyler Jang <tyler@trunk.io>
2024-04-09 23:20:09 -0700

    Cleanup infra issues (#736)
    - `remark-lint`'s `extra_packages` released new changes, so I've
    refreshed the snapshots. We don't pin versions unless explicit, so this
    causes some mild hermeticity issues (but is still an acceptable edge
    case)
    - Change to use `ubuntu-latest` runners until we find a more sustainable
    solution for this repo (see nightly failures)
    - Mild workflow cleanup
    
    Takes over #725 and addresses [nightly
    issues](https://github.com/trunk-io/plugins/actions/runs/8612368048)

--- linters/renovate/renovate.test.ts
+++ linters/renovate/renovate.test.ts
@@ -1,7 +1,7 @@
 import { customLinterCheckTest } from "tests";
 import { osTimeoutMultiplier, TEST_DATA } from "tests/utils";
 
-jest.setTimeout(600000 * osTimeoutMultiplier); // 300s or 900s
+jest.setTimeout(600000 * osTimeoutMultiplier);
 
 customLinterCheckTest({
   linterName: "renovate",
