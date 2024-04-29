536765d8978bcdd6ced7f407f841c48fc5341954
Vincent (Wen Yu) Ge <vincent@appwrite.io>
2024-04-28 20:17:03 -0400

    Feat: Update tool to use Vale binary and add success and issue tests for linter

--- linters/vale/vale.test.ts
+++ linters/vale/vale.test.ts
@@ -1,8 +1,24 @@
-import { customLinterCheckTest } from "tests";
+import * as path from "path";
+import { customLinterCheckTest, fuzzyLinterCheckTest } from "tests";
 import { TEST_DATA } from "tests/utils";
+import { createFuzzyMatcher } from "tests/utils/landing_state";
+import * as fs from "fs";
+import { FileIssue, LandingState } from "tests/types";
 
+// Specify a path and file to avoid accidentally
 customLinterCheckTest({
   linterName: "vale",
   testName: "basic",
-  args: TEST_DATA,
+  args: path.join(TEST_DATA, "documents/README.md"),
+});
+
+const expectedBasicFileIssues = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "expected_issues.json")).toString(),
+);
+
+fuzzyLinterCheckTest({
+  linterName: "vale",
+  testName: "poor spelling",
+  args: path.join(TEST_DATA, "documents/BAD_FILE.md"),
+  fileIssueAssertionCallback: createFuzzyMatcher(() => expectedBasicFileIssues as FileIssue[], 2),
 });
