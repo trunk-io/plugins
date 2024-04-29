1330da2f3ad17ef356761d436ed68c8200fc38d9
Vincent (Wen Yu) Ge <vincent@appwrite.io>
2024-04-29 09:45:33 -0400

    fix: move tool definition into linter, simplify tests

--- linters/vale/vale.test.ts
+++ linters/vale/vale.test.ts
@@ -1,24 +1,3 @@
-import * as path from "path";
-import { customLinterCheckTest, fuzzyLinterCheckTest } from "tests";
-import { TEST_DATA } from "tests/utils";
-import { createFuzzyMatcher } from "tests/utils/landing_state";
-import * as fs from "fs";
-import { FileIssue, LandingState } from "tests/types";
+import { linterCheckTest } from "tests";
 
-// Specify a path and file to avoid linting a bad file on a happy case.
-customLinterCheckTest({
-  linterName: "vale",
-  testName: "basic",
-  args: path.join(TEST_DATA, "documents/README.md"),
-});
-
-const expectedBasicFileIssues = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, "expected_issues.json")).toString(),
-);
-
-fuzzyLinterCheckTest({
-  linterName: "vale",
-  testName: "poor spelling",
-  args: path.join(TEST_DATA, "documents/BAD_FILE.md"),
-  fileIssueAssertionCallback: createFuzzyMatcher(() => expectedBasicFileIssues as FileIssue[], 2),
-});
+linterCheckTest({ linterName: "vale" });
--- tools/vale/pnpm.test.ts
+++
@@ -1,9 +0,0 @@
-import { makeToolTestConfig, toolTest } from "tests";
-
-toolTest({
-  toolName: "vale",
-  toolVersion: "3.4.1",
-  testConfigs: [
-    makeToolTestConfig({ command: ["vale", "--version"], expectedOut: "vale version 3.4.1" }),
-  ],
-});
