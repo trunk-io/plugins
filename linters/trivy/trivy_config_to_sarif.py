ac24ac1252fa0c8c5deddeb2436e500bd0e27f62
Tyler Jang <tyler@trunk.io>
2024-05-10 14:57:05 -0700

    Fix trivy parsing for terraform (#775)
    Otherwise we were getting results on line 130 for a 24 line file. Added
    regression test as well.
    
    Also adds comment style for terraform filetype.

--- linters/trivy/trivy_config_to_sarif.py
+++ linters/trivy/trivy_config_to_sarif.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import json
+import os
 import sys
 
 
@@ -29,6 +30,7 @@ def to_result_sarif(path: str, vuln_id: str, description: str, line: int = 0):
 
 def main(argv):
     trivy_json = json.load(sys.stdin)
+    path = trivy_json["ArtifactName"]
     results = []
 
     for result in trivy_json.get("Results", []):
@@ -37,11 +39,15 @@ def main(argv):
         for vuln in result["Misconfigurations"]:
             vuln_id = vuln["ID"]
             message = vuln["Message"]
-            line_num = vuln.get("CauseMetadata", {}).get("StartLine", 0)
+            location_info = vuln.get("CauseMetadata", {})
+            line_num = location_info.get("StartLine", 0)
+            for occurrence in location_info.get("Occurrences", []):
+                # Sometimes the original StartLine will reference a resource file
+                if occurrence.get("Filename") == os.path.basename(path):
+                    line_num = occurrence["Location"]["StartLine"]
+                    break
 
-            results.append(
-                to_result_sarif(trivy_json["ArtifactName"], vuln_id, message, line_num)
-            )
+            results.append(to_result_sarif(path, vuln_id, message, line_num))
 
     sarif = {
         "$schema": "https://raw.githubusercontent.com/oasis-tcs/sarif-spec/master/Schemata/sarif-schema-2.1.0.json",
