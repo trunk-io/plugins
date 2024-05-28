f228f49cce050c62488e727931c95dff8aa67213
Matthew Baggett <matthew@baggett.me>
2024-05-28 18:22:19 +0100

    Adding some of the most common php tooling  (#757)
    * phpunit for running unittests
    * paratest for running the same unitests in parallel
    * php-cs-fixer for enforcing formatting. 
    
    php-cs-fixer could be considered to be promoted to being a linter as
    soon as I work out how to parse its output and feed it through a
    post-processor like phpstan is.
    
    ---------
    
    Co-authored-by: Tyler Jang <tyler@trunk.io>

--- tools/gk/gk.test.ts
+++ tools/gk/gk.test.ts
@@ -1,5 +1,6 @@
 import { toolInstallTest } from "tests";
 
+// TODO(Tyler): tool def is missing healthcheck
 toolInstallTest({
   toolName: "gk",
   toolVersion: "1.2.2",
--- tools/goreleaser/goreleaser.test.ts
+++ tools/goreleaser/goreleaser.test.ts
@@ -4,6 +4,7 @@ import { osTimeoutMultiplier } from "tests/utils";
 // This install is quite slow on some Linux machines.
 jest.setTimeout(600000 * osTimeoutMultiplier);
 
+// TODO(Tyler): tool def is missing healthcheck
 toolInstallTest({
   toolName: "goreleaser",
   toolVersion: "1.25.1",
