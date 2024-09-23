04abdcdf9662d0983009eebda578c8c443b67017
Tyler Jang <tyler@trunk.io>
2024-09-22 21:46:06 -0700

    fix squawk test

--- linters/squawk/squawk.test.ts
+++ linters/squawk/squawk.test.ts
@@ -1,7 +1,7 @@
 import { linterCheckTest } from "tests";
 
-// No release for squawk on arm64 darwin https://github.com/sbdchd/squawk/issues/372
+// No release for squawk on arm64 linux https://github.com/sbdchd/squawk/issues/372
 linterCheckTest({
   linterName: "squawk",
-  skipTestIf: () => process.arch === "arm64" && process.platform === "darwin",
+  skipTestIf: () => process.arch === "arm64" && process.platform === "linux",
 });
