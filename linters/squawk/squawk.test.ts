b30e16ff7411ab833ca671c0402ccefb91af8dfb
Tyler Jang <tyler@trunk.io>
2024-09-24 19:26:49 -0700

    turn on for linux arm

--- linters/squawk/squawk.test.ts
+++ linters/squawk/squawk.test.ts
@@ -1,7 +1,12 @@
+import semver from "semver";
 import { linterCheckTest } from "tests";
 
-// No release for squawk on arm64 linux https://github.com/sbdchd/squawk/issues/372
+// Squawk first supported linux arm in 1.4.0 https://github.com/sbdchd/squawk/issues/372
 linterCheckTest({
   linterName: "squawk",
-  skipTestIf: () => process.arch === "arm64" && process.platform === "linux",
+  skipTestIf: (version) =>
+    process.arch === "arm64" &&
+    process.platform === "linux" &&
+    version !== undefined &&
+    semver.lt(version, "1.4.0"),
 });
