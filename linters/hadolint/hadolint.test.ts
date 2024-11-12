2fcbf624eb8a7a52c241f7f11eebb0232f507be8
Chris Clearwater <chris@trunk.io>
2024-11-12 13:36:31 -0800

    Fix hadolint on some versions of macOS (#913)

--- linters/hadolint/hadolint.test.ts
+++ linters/hadolint/hadolint.test.ts
@@ -1,5 +1,15 @@
 import { customLinterCheckTest } from "tests";
 import { TEST_DATA } from "tests/utils";
 
+const manualVersionReplacer = (version: string) => {
+  if (version === "2.12.0") {
+    // This version may segfault on certain versions on macOS.
+    // The beta version is identical except with upx compression disabled on the binary.
+    // See https://github.com/hadolint/hadolint/issues/919#issuecomment-1672031271
+    return "2.12.1-beta";
+  }
+  return version;
+};
+
 // We have a number of different files to determine which are valid Dockerfiles, validated by lint actions in the snapshot
-customLinterCheckTest({ linterName: "hadolint", args: TEST_DATA });
+customLinterCheckTest({ linterName: "hadolint", args: TEST_DATA, manualVersionReplacer });
