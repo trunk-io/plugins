9f9a18a131b8c9b727889d6cfb27b7c68eb74ebf
Felix Sargent <felix.sargent@snyk.io>
2025-01-20 16:29:02 +0000

    Add some test data, but can't get tests to pass.

--- linters/snyk/snyk.test.ts
+++ linters/snyk/snyk.test.ts
@@ -1,5 +1,3 @@
-import { linterCheckTest, linterFmtTest } from "tests";
-
 // Guidelines for configuring tests:
 //  - By default, linters and formatters will only run on files with syntax `<name>.in.<extension>`
 //  - You can customize test setup using the `preCheck` callback (see git_diff_check.test.ts and golangci_lint.test.ts)
@@ -8,8 +6,6 @@ import { linterCheckTest, linterFmtTest } from "tests";
 //
 // If you are unable to write a test for this linter, please document why in your PR. Feel free to ask for help!
 
-// Use if your tool is a linter
-linterCheckTest({ linterName: "snyk" });
-
-// Use if your tool is a formatter
-linterFmtTest({ linterName: "snyk" });
+import { customLinterCheckTest } from "tests";
+import { TEST_DATA } from "tests/utils";
+customLinterCheckTest({ linterName: "snyk", args: TEST_DATA, testName: "basic" });
