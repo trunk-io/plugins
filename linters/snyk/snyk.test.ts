d8886e33b6d2286b9cf0e4640b8e09cce48d3cad
Tyler Jang <tyler@trunk.io>
2025-01-22 00:00:43 -0800

    add fixes and test setup

--- linters/snyk/snyk.test.ts
+++ linters/snyk/snyk.test.ts
@@ -1,11 +1,4 @@
-// Guidelines for configuring tests:
-//  - By default, linters and formatters will only run on files with syntax `<name>.in.<extension>`
-//  - You can customize test setup using the `preCheck` callback (see git_diff_check.test.ts and golangci_lint.test.ts)
-//  - You can specify additional customization using the `customLinterCheckTest` and `customLinterFmtTest` helpers
-//  - Additional information on test setup can be found in tests/README.md
-//
-// If you are unable to write a test for this linter, please document why in your PR. Feel free to ask for help!
-
 import { customLinterCheckTest } from "tests";
 import { TEST_DATA } from "tests/utils";
+
 customLinterCheckTest({ linterName: "snyk", args: TEST_DATA, testName: "basic" });
