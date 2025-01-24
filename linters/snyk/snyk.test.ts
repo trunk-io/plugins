9210b56d8554c5c678a2f9d12d17b3b53c98f5f5
Tyler Jang <tyler@trunk.io>
2025-01-24 10:48:38 -0800

    test snyk

--- linters/snyk/snyk.test.ts
+++ linters/snyk/snyk.test.ts
@@ -1,4 +1,5 @@
 import { customLinterCheckTest } from "tests";
 import { TEST_DATA } from "tests/utils";
 
+// Requires SNYK_TOKEN to run
 customLinterCheckTest({ linterName: "snyk", args: TEST_DATA, testName: "basic" });
