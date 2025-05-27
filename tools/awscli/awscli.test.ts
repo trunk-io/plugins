4b793748c48a555e9ad1adad891862da961a8484
Eli Schleifer <1265982+EliSchleifer@users.noreply.github.com>
2025-05-26 22:01:27 -0700

    Add additional environment variables used by python networking stack (#1032)
    Add additional environment variables used by python networking stack
    
    Passes thru additional environment variable to PIP to unblock pip and
    other PYTHON libraries that use proxy info. Fixes broken trunk check in
    OpenAI Codex

--- tools/awscli/awscli.test.ts
+++ tools/awscli/awscli.test.ts
@@ -3,11 +3,11 @@ import { skipOS } from "tests/utils";
 
 toolTest({
   toolName: "awscli",
-  toolVersion: "1.29.30",
+  toolVersion: "1.40.22",
   testConfigs: [
     makeToolTestConfig({
       command: ["aws", "--version"],
-      expectedOut: "aws-cli/1.29.30",
+      expectedOut: "aws-cli/1.40.22",
     }),
   ],
   // On Windows, the shim is aws.cmd, and we don't support platform-specific shims yet.
