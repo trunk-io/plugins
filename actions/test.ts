8a1df9df533294f8615ab1b55f00ddfb37af3f03
marschattha <arslan_21@rocketmail.com>
2025-09-30 23:55:29 +0500

    Test pr branch

---
+++ actions/test.ts
@@ -0,0 +1,36 @@
+import { actionRunTest } from "tests";
+import { TrunkActionDriver } from "tests/driver";
+
+const preCheck = (driver: TrunkActionDriver) => {
+  driver.writeFile(
+    "commitlint.config.mjs",
+    "export default {extends: ['@commitlint/config-conventional']}",
+  );
+};
+
+const testCallback = async (driver: TrunkActionDriver) => {
+  try {
+    await driver.gitDriver?.commit(
+      "Test commit",
+      [],
+      { "--allow-empty": null },
+      (error, result) => {
+        expect(error?.message).toContain("subject may not be empty [subject-empty]");
+        expect(error?.message).toContain("type may not be empty [type-empty]");
+        expect(result).toBeUndefined();
+      },
+    );
+
+    // Commit step should throw
+    expect(1).toBe(2);
+  } catch (_err) {
+    // Intentionally empty
+  }
+};
+
+actionRunTest({
+  actionName: "commitlint",
+  syncGitHooks: true,
+  testCallback,
+  preCheck,
+});
