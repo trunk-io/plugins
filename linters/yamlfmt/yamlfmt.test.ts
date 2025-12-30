79c051b4b6a4ccab5a77b843718d275768a326de
Kevin Mahoney <belay.00.theorem@icloud.com>
2025-12-29 19:42:13 -0500

    Ensure yamlfmt respects trunk-managed configs

--- linters/yamlfmt/yamlfmt.test.ts
+++ linters/yamlfmt/yamlfmt.test.ts
@@ -1,9 +1,25 @@
 import { linterCheckTest, linterFmtTest } from "tests";
+import { TrunkLintDriver } from "tests/driver";
 import { osTimeoutMultiplier } from "tests/utils";
 
 // This install is quite slow on some Linux machines.
 jest.setTimeout(600000 * osTimeoutMultiplier);
 
+const writeTrunkConfig = (driver: TrunkLintDriver) => {
+  const config = `formatter:
+  type: basic
+  scan_folded_as_literal: true
+  retain_line_breaks: true
+`;
+  driver.writeFile(".trunk/configs/.yamlfmt.yml", config);
+};
+
 linterCheckTest({ linterName: "yamlfmt", namedTestPrefixes: ["empty"] });
 
 linterFmtTest({ linterName: "yamlfmt", namedTestPrefixes: ["basic"] });
+
+linterFmtTest({
+  linterName: "yamlfmt",
+  namedTestPrefixes: ["config"],
+  preCheck: writeTrunkConfig,
+});
